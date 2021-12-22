af9bbd16747f2d0bcc690e55b4af37effbaca9fc
channylim24 <channylim24@gmail.com>
2021-12-23 00:37:31 +0700

    fix: login

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from "redux";
 import studyRoomReducer from "./studyRoomReducer";
 import homeRoomReducer from "./homeRoomReducer";
 import formReducer from "./formReducer";
+import authReducer from "./authReducer";
 
 const rootReducer = combineReducers({
   reducers: "",
   studyRoomReducer,
   homeRoomReducer,
   formReducer,
+  authReducer,
 });
 
 export default rootReducer;
